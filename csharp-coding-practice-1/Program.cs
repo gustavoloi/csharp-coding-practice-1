ba4d34302916b68fad2da66e4fddd0db64385ef2
Gustavo <gustavoloi@hotmail.com>
2021-07-19 15:21:30 -0400

    Add project files.

---
+++ csharp-coding-practice-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_coding_practice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
