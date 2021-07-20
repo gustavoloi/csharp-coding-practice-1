dd6364ecf0a4cfad15e01e1c8424ea6bdfdb0f87
Gustavo <gustavoloi@hotmail.com>
2021-07-20 01:03:39 -0400

    The 0 gets sent to the middle of the array. Still need some final touches to make it more polished. Details on comments.

--- csharp-coding-practice-1/Program.cs
+++ csharp-coding-practice-1/Program.cs
@@ -1,4 +1,12 @@
-﻿using System.CodeDom.Compiler;
+﻿/*
+ TODO LIST
+-CHECKS FOR AN EXISTING 0 IN ANY OTHER WAY. HOW IT IS RIGHT NOW IS NOT GOOD ENOUGH
+-CHECK FOR MORE THAN 1 ZERO.
+    -MAYBE COMPARE IF THE NUMBER IN THE CURRENT POSITION IS ALREADY A ZERO? THEN WHAT?
+-DO ANYTHING WITH THE OTHER NUMBERS?
+ */
+
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Collections;
 using System.ComponentModel;
@@ -24,7 +32,7 @@ namespace csharp_coding_practice_1
             var rand = new Random(); //Create a random object
             resultList.Add(0); //Makes sure at least one "0" is in the list
 
-            for (int i = 0; i < listSize; i++) //Loop to add items to the list based on the size typed
+            for (int i = 1; i < listSize; i++) //Loop to add items to the list based on the size typed
             {
                 resultList.Add(rand.Next(10)); //100 indicates the range of values
             }
@@ -36,6 +44,32 @@ namespace csharp_coding_practice_1
         public static List<int> ZeroesToCenter(List<int> arr)
         {
             List<int> displayList = new List<int>();
+            int oldIndex;
+            int newIndex = arr.Count / 2;
+            int numberToArrange;
+            int temp;
+            Console.WriteLine("The target index is: " + newIndex.ToString());
+            int index = 0;
+
+
+
+            foreach (var item in arr.ToList())
+            {
+
+                if (item == 0) // Moves the position if the current number(item) is 0.
+                {
+                    oldIndex = index;
+                    //temp = ;
+                    arr.RemoveAt(newIndex);
+                    arr.Insert(newIndex, 0);
+                    //arr[newIndex] = 0;
+                    //arr[index] = 1;
+                    //arr.
+                    // Get the current index of the item
+                    //arr.
+                }
+                index++;
+            }
 
             return displayList;
         }
@@ -52,18 +86,21 @@ namespace csharp_coding_practice_1
             arr = Result.RandomNumberGenerator(10);
 
 
-            foreach (var item in arr) //Display all the results
-            {
-                Console.WriteLine(item);
-            }
 
 
             //Save this section for later, after the array is organized
-            //List<int> result = Result.ZeroesToCenter(arr);
-            //foreach(var item in result)
+            List < int > result = Result.ZeroesToCenter(arr);
+
+            foreach (var item in arr)
+                {
+                    Console.Write("{0} ", item);
+                }
+
+            //foreach (var item in arr) //Display all the results
             //{
-            //    Console.WriteLine(item);
+            //    Console.Write(item);
             //}
+
         }
     }
 }
